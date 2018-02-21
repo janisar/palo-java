407992c325608a49e78d03adefdbd71269b09168
Jacob <jacob@iwannaticket.com.au>
2018-02-21 12:11:47 +1100

    Initial commit

---
+++ src/main/java/ee/ardel/paloserver/PaloServerApplication.java
@@ -0,0 +1,12 @@
+package ee.ardel.paloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaloServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaloServerApplication.class, args);
+	}
+}
